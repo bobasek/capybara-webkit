2d6534fe9911e7e24dc45b1738398c8832e75b81
root <root@ip-10-146-193-158.ec2.internal>
2014-03-05 15:14:33 +0000

    fixes

--- lib/capybara/webkit/connection.rb
+++ lib/capybara/webkit/connection.rb
@@ -42,8 +42,9 @@ module Capybara::Webkit
     end
 
     def open_pipe
-      _, @pipe_stdout, @pipe_stderr, wait_thr = Open3.popen3(SERVER_PATH)
-      @pid = wait_thr[:pid]
+      @pipe_stdin, @pipe_stdout, @pipe_stderr, @wait_thr = Open3.popen3(SERVER_PATH)
+      
+      @pid = @wait_thr[:pid]
       register_shutdown_hook
     end
 
@@ -76,7 +77,7 @@ module Capybara::Webkit
         @socket.close
       rescue IOError
       end
-    
+   end 
       # This just means that the webkit_server process has already ended
     end
 
